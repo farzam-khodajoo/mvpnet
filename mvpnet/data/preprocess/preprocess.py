6abb3b8f497efae97dc09a2c81b13877469efdca
Farzam Khodajoo <arsham.khodajoo@gmail.com>
2023-08-10 23:18:32 +0330

    add late night changes, no energy to explain

--- mvpnet/data/preprocess/preprocess.py
+++ mvpnet/data/preprocess/preprocess.py
@@ -16,6 +16,22 @@ from plyfile import PlyData
 from PIL import Image
 import open3d as o3d
 
+# this will be to root of all dataset, which means 2D dataset, 
+# metafiles and cached samples will be stored.
+# after the process has been completed, this directory will contain a structure like this:
+# - /content/dataset
+# - - - resized_scans_120_160/
+# - - - pickles/
+# - - - metafiles/
+ROOT_DIRECTORY = "/content/dataset"
+# this directory is where you downloaded scannet dataset to.
+# which should contain samples named in this format: scene0**_**
+# this folder can also be inside ROOT_DIRECTORY, as it makes no difference.
+SCANNET_DIRECTORY = "/content/dataset/scans"
+# choose how many processing core you would like to be used.
+MULTI_CORE_PROCESSING = 1
+
+
 # DATA_DIR = '/datasets_local/ScanNet'
 # SCAN_DIR = 'scans_resize_160x120'
 # META_DIR = '/home/docker_user/workspace/mvpnet_private/mvpnet/data/meta_files'
